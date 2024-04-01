3b6cce3fefb50c7d16ea4dde8bea61dd7bc8ad60
Juan Alexandro Renteria Abril <97640975+YuanitoJones@users.noreply.github.com>
2024-03-31 20:25:18 -0700

    added nextui

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+// `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
+
+import Link from "next/link";
+
+export default function Page() {
+    return <section>Hello, Dashboard Page!</section>
+  }
